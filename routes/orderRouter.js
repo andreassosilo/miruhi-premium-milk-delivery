d9e0378cdaaf298fe5a933d6b8bc8a7539dbe7ec
andreassosilo <andreas.sosilo@gmail.com>
2019-08-30 09:32:14 +0700

    Modify orderRouter.js

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -3,12 +3,13 @@
 const express = require('express')
 const router = express.Router()
 const { OrderController } = require('../controllers')
+const isLogin = require('../middlewares/isLogin')
 
-router.get('/create', OrderController.showOrderForm)
-router.post('/create', OrderController.create)
+router.get('/create', isLogin, OrderController.showOrderForm)
+router.post('/create', isLogin, OrderController.create)
 // router.get('/update', OrderController.showUpdateForm)
 // router.post('/update', OrderController.updateOrder)
-router.get('/view', OrderController.showOrderInformation)
+router.get('/view', isLogin, OrderController.showOrderInformation)
 router.get('/find', OrderController.showFindOrder)
 
 module.exports = router
